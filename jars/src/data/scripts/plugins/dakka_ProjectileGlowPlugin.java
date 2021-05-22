a44ed4c10543702672d148360e21079e9bd7fec5
Nia Tahl <eliyanbrize@gmail.com>
2021-05-22 13:51:26 +0200

    Initial commit

---
+++ jars/src/data/scripts/plugins/dakka_ProjectileGlowPlugin.java
@@ -0,0 +1,13 @@
+package data.scripts.plugins;
+
+import com.fs.starfarer.api.combat.BaseEveryFrameCombatPlugin;
+import com.fs.starfarer.api.input.InputEventAPI;
+
+import java.util.List;
+
+public class dakka_ProjectileGlowPlugin extends BaseEveryFrameCombatPlugin {
+    @Override
+    public void advance(float amount, List<InputEventAPI> events) {
+
+    }
+}
---
+++ jars/src/data/scripts/weapons/dakka_PlasmaGlowEffect.java
@@ -0,0 +1,52 @@
+package data.scripts.weapons;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.combat.*;
+import com.sun.scenario.Settings;
+import data.scripts.util.MagicRender;
+import org.lazywizard.lazylib.combat.CombatUtils;
+import org.lwjgl.util.vector.Vector2f;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class dakka_PlasmaGlowEffect implements EveryFrameWeaponEffectPlugin, OnFireEffectPlugin {
+
+    private final List<DamagingProjectileAPI> registeredProjectiles = new ArrayList<>();
+
+    @Override
+    public void onFire(DamagingProjectileAPI projectile, WeaponAPI weapon, CombatEngineAPI engine) {
+        registeredProjectiles.add(projectile);
+    }
+
+    @Override
+    public void advance(float amount, CombatEngineAPI engine, WeaponAPI weapon) {
+
+        List<DamagingProjectileAPI> toRemove = new ArrayList<>();
+
+        for (DamagingProjectileAPI proj : registeredProjectiles) {
+            //Ignore already-collided projectiles, and projectiles that don't match our IDs
+            if (proj.getProjectileSpecId() == null || proj.didDamage() || !engine.isEntityInPlay(proj)) {
+                toRemove.add(proj);
+                continue;
+            }
+
+            MagicRender.singleframe(
+                    Global.getSettings().getSprite("fx","tahlan_dakka_glow"),
+                    proj.getLocation(),
+                    new Vector2f(200f,200f),
+                    proj.getFacing(),
+                    new Color(246, 62, 122, 54),
+                    true
+            );
+        }
+
+        for (DamagingProjectileAPI proj : toRemove) {
+            registeredProjectiles.remove(proj);
+        }
+    }
+
+
+
+}
